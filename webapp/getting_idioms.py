421e7b7745898e0217f346ac456852a176fcc049
Julia <bezymyannikova@gmail.com>
2019-03-24 15:53:27 +0300

    Доработала скрипт, чтобы обработать не 1 тему, а все

--- webapp/getting_idioms.py
+++ webapp/getting_idioms.py
@@ -11,8 +11,27 @@ def get_html(url):
         print('Сетевая ошибка')
         return False
 
-def get_python_idioms():
-    html = get_html('https://www.native-english.ru/idioms/category/leg-and-foot/')
+
+def get_links_to_themes():
+    html = get_html('https://www.native-english.ru/idioms/')
+    if html:
+        soup = BeautifulSoup(html, 'html.parser')
+        all_idioms = soup.findAll('ul', class_="inlist inlist_loose")[1].findAll('li', class_="inlist__item")
+        result_list_of_themes = []
+        for idiom in all_idioms: 
+            name_of_theme = (idiom.find('a').text).capitalize().strip()
+            link_to_theme = 'https://www.native-english.ru' + idiom.find('a')['href']
+            name_of_csv = name_of_theme.lower().replace(' ', '_') + '.csv'
+            result_list_of_themes.append({
+                'name_of_theme': name_of_theme,
+                'link_to_theme': link_to_theme,
+                'name_of_csv': name_of_csv
+            })
+        return result_list_of_themes
+    return False
+
+def get_python_idioms(link):
+    html = get_html(link)
     if html:
         soup = BeautifulSoup(html, 'html.parser')
         all_idioms = soup.find('ul', class_="list list_big").findAll('li', class_="list__item pane")
@@ -30,14 +49,12 @@ def get_python_idioms():
     return False
 
 if __name__ == "__main__":
-        html = get_html("https://www.native-english.ru/idioms/category/leg-and-foot/")
-        if html:
-            with open("idioms.html", "w", encoding="utf8") as f:
-                f.write(html)
-        idioms_list = get_python_idioms()
-        with open('leg_and_foot.csv', 'w', encoding='utf-8', newline='') as user_file:
-            fields = ['name_of_idiom', 'translation', 'definition']
-            writer = csv.DictWriter(user_file, fields, delimiter=';')
-            writer.writeheader()
-            for idiom in idioms_list:
-                writer.writerow(idiom)
+        list_of_themes = get_links_to_themes()
+        for theme in list_of_themes:
+            idioms_list = get_python_idioms(theme['link_to_theme'])
+            with open(theme['name_of_csv'], 'w', encoding='utf-8', newline='') as user_file:
+                fields = ['name_of_idiom', 'translation', 'definition']
+                writer = csv.DictWriter(user_file, fields, delimiter=';')
+                writer.writeheader()
+                for idiom in idioms_list:
+                    writer.writerow(idiom)
