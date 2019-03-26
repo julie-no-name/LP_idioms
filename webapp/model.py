9ea8d0d688317547ae104f0c65834e9f0aff4cb2
Julia <bezymyannikova@gmail.com>
2019-03-26 23:50:43 +0300

    поправила ошибку в модели

--- webapp/model.py
+++ webapp/model.py
@@ -4,9 +4,10 @@ db = SQLAlchemy()
 
 class Idioms(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    name_of_idiom = db.Column(db.Text, unique = True)
-    translation = db.Column(db.Text, unique = True)
-    definition = db.Column(db.Text, unique = True)
+    name_of_theme = db.Column(db.String, unique = True, nullable=False)
+    name_of_idiom = db.Column(db.String, unique = True, nullable=False)
+    translation = db.Column(db.String, nullable=False)
+    definition = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
         return f'<Idioms {self.name_of_idiom} {self.translation} {self.definition}>'
