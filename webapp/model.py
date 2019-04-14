4e057f39f91cea85e999e7e6aac3eb1324c58d7b
icewem <icewem@gmail.com>
2019-04-14 15:31:37 +0300

    добавил базу данных с пользователями и поправил в БД с идиомами индексацию

--- webapp/model.py
+++ webapp/model.py
@@ -5,13 +5,20 @@ db = SQLAlchemy()
 class Idioms(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
-    name_of_theme = db.Column(db.String, nullable=False)
-    name_of_idiom = db.Column(db.String, unique = True, nullable=False)
-    translation = db.Column(db.String, nullable=False)
-    definition = db.Column(db.Text, nullable=False)
+    name_of_theme = db.Column(db.String, nullable=False,index=True)
+    name_of_idiom = db.Column(db.String, unique = True, nullable=False, index=True)
+    translation = db.Column(db.String, nullable=False, index=True)
+    definition = db.Column(db.Text, nullable=False, index=True)
 
     def __repr__(self):
         return f'<Idioms {self.name_of_idiom} {self.translation} {self.definition}>'
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password = db.Column(db.String(128))
+    role = db.Column(db.String(10), index=True)
 
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
