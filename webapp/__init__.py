82c51f73795a33f8f2a913e206f9430bfbf8d4d9
Julia <julia@Air-Julia.localdomain>
2019-04-17 18:36:29 +0300

    Выводятся идиомы и переводы

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
 from webapp.model import db, Idioms
 from webapp.forms import SearchForm, LoginForm
+import random
+from sqlalchemy.sql import func
+
 
 
 def create_app():
@@ -25,6 +28,41 @@ def create_app():
         page_title = "Login"
         login_form = LoginForm()
         return render_template('login.html', page_title=page_title, form=login_form)
+
+    @app.route('/practice')
+    def practice():
+        page_title = "Тренировка"
+        variants = []
+        random_cell = Idioms.query.order_by(func.random()).first()
+        random_idiom = random_cell.name_of_idiom
+        variants.append(random_cell.translation)
+        variants.append(Idioms.query.order_by(func.random()).first().translation)
+        variants.append(Idioms.query.order_by(func.random()).first().translation)
+        variants.append(Idioms.query.order_by(func.random()).first().translation)
+        random.shuffle(variants)
+        idioms_list = Idioms.query
+        
+ 
+        return render_template('practice.html', page_title=page_title, idioms_list=idioms_list.all(),
+            random_idiom=random_idiom, variants=variants)
+
+    
+    @app.route('/answer')
+    def answer():
+        page_title = "Тренировка"
+        variants = []
+        random_cell = Idioms.query.order_by(func.random()).first()
+        random_idiom = random_cell.name_of_idiom
+        variants.append(random_cell.translation)
+        variants.append(Idioms.query.order_by(func.random()).first().translation)
+        variants.append(Idioms.query.order_by(func.random()).first().translation)
+        variants.append(Idioms.query.order_by(func.random()).first().translation)
+        random.shuffle(variants)
+        idioms_list = Idioms.query
+        
+ 
+        return render_template('answer.html', page_title=page_title, idioms_list=idioms_list.all(),
+            random_idiom=random_idiom, variants=variants)
 
 
     return app
